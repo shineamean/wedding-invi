4dfab647be33bc81c8125d3b33c5db8f974b5788
jihwan.kim <shineamean@snu.ac.kr>
2025-08-20 21:24:08 +0900

    add env setting

---
+++ src/firebase-config.ts
@@ -0,0 +1,14 @@
+// Firebase 설정 파일
+// 실제 Firebase 프로젝트 설정 (wedding-33caf)
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDevIFGJaMxRqOC1jmzX4Ucj7KpscqWaFY",
+  authDomain: "wedding-33caf.firebaseapp.com",
+  projectId: "wedding-33caf",
+  storageBucket: "wedding-33caf.firebasestorage.app",
+  messagingSenderId: "392686404524",
+  appId: "1:392686404524:web:1e500d4bf714a4ea8df009",
+  measurementId: "G-C3DYN2JQ6W"
+}
+
+// 참고: Firebase 초기화는 Guestbook 컴포넌트에서 처리됩니다.
